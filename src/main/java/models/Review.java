e725a607d8e420f31cd93aea63299fae7550b400
Tony <NodeToNowhere@gmail.com>
2022-02-25 09:54:34 +0000

    Repo's + Review model

---
+++ src/main/java/models/Review.java
@@ -0,0 +1,78 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "upvotes")
+    private int upvotes;
+
+    @ManyToOne
+    @JsonIgnoreProperties({"reviews"})
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JsonIgnoreProperties({"channel"})
+    @JoinColumn(name = "channel_id", nullable = false)
+    private Channel channel;
+
+    public Review(String comment, int upvotes, User user, Channel channel) {
+        this.comment = comment;
+        this.upvotes = upvotes;
+        this.user = user;
+        this.channel = channel;
+    }
+
+    public Review() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getUpvotes() { return upvotes;}
+
+    public void setUpvotes(int upvotes) {this.upvotes = upvotes;}
+
+    public User getCustomer() {
+        return user;
+    }
+
+    public void setCustomer(User user) {
+        this.user = user;
+    }
+
+    public Channel getCourse() {
+        return channel;
+    }
+
+    public void setCourse(Channel course) {
+        this.channel = course;
+    }
+}
