90cdaa7bfef83a266bfd5b56869a128665cf8c45
Tony <NodeToNowhere@gmail.com>
2022-02-25 09:20:33 +0000

    Initial Springdb

---
+++ src/test/java/com/SoloGroup/TwitchReview/TwitchReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoloGroup.TwitchReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
