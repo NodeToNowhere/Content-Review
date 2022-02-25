f3f185e68729936c79fb55b2db764284fc52da94
Tony <NodeToNowhere@gmail.com>
2022-02-25 10:35:14 +0000

    Channel Model

---
+++ src/main/java/models/Channel.java
@@ -0,0 +1,69 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "channel")
+public class Channel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "rating")
+    private int rating;
+
+
+    @JsonIgnoreProperties({"channel"})
+    @OneToMany(mappedBy = "channel", fetch = FetchType.LAZY)
+    private List<Review> reviews;
+
+    public Channel(String name, int rating) {
+        this.name = name;
+        this.rating = rating;
+        this.reviews = new ArrayList<Review>();
+    }
+
+    public Channel(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+}
