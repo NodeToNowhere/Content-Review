9b2c55238af07d8e8f1e41b784d50025c455a1ad
Tony <NodeToNowhere@gmail.com>
2022-02-28 17:02:54 +0000

    Body component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import ChannelPage from './views/ChannelPage';
-import UserPage from './views/UserPage';
-import SearchPage from './views/SearchPage';
-import LandingPage from './views/LandingPage';
-import Sidebar from './components/SideBar';
-const API_KEY = process.env.REACT_API_KEY;
 
+import React from "react";
+import "./App.css";
+import Header from "./components/Header"
+import Body from "./components/Body";
+import Sidebar from "./components/SideBar";
+function App() {
+  return <div className="App"> 
+  <Header />
+  <Sidebar />
+  <Body />
 
-
-
-const App = () => {
-  return (
-      <div>
-          <BrowserRouter>
-              <div>
-                  <Header />
-                  <Sidebar />
-                  <Routes>
-                  <Route path="/" exact component={LandingPage} />
-                        <Route path="/channels" exact component={ChannelPage} />
-                        <Route path="/users" exact component={UserPage} />
-                        <Route path="/search" exact component={SearchPage} />
-                        </Routes>
-              </div>
-          </BrowserRouter>
-      </div>
-  );
+  </div>;
 }
-
 export default App;
-
-
--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -6,7 +6,7 @@ const Sidebar = () => {
     <div className='sidebar' >
       <div className="sidebar__top">
         
-        <h5>FOLLOWED CHANNELS</h5>
+        <h5>Suggested Reviews</h5>
         <div className="sidebar__bottomContainer">
           <i className="fas fa-search"></i>
           <input type="text" placeholder='Search to Add Favourites' />
