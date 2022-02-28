52a1c93e384081c4a3268010e94bce86a3ce9a6c
Tony <NodeToNowhere@gmail.com>
2022-02-28 14:31:11 +0000

    refactor + tidy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,9 +18,9 @@ const App = () => {
                   <Header />
                   <Routes>
                   <Route path="/" exact component={LandingPage} />
-                        <Route path="/ChannelPage" exact component={ChannelPage} />
-                        <Route path="/UserPage" exact component={UserPage} />
-                        <Route path="/SearchPage" exact component={SearchPage} />
+                        <Route path="/channels" exact component={ChannelPage} />
+                        <Route path="/users" exact component={UserPage} />
+                        <Route path="/search" exact component={SearchPage} />
                         </Routes>
               </div>
           </BrowserRouter>
